ea6952ea9d93be74e6e49434f6adbf346b7eae58
AndreaHollub <baponce@uc.cl>
2016-06-22 11:00:14 -0400

    version arreglada

---
+++ CineramApp 0.1/CineramApp 0.1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CineramApp_0._1.MvcApplication" Language="C#" %>
---
+++ CineramApp 0.1/CineramApp 0.1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CineramApp_0._1.Startup))]
+namespace CineramApp_0._1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
